e76e984d9ed01673348d90e346d04f7faed06713
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-06-30 11:02:47 +0530

    changed the network setting

--- src/main.rs
+++ src/main.rs
@@ -44,8 +44,7 @@ async fn main() {
 
     println!("🚀 Server running at http://localhost:3000");
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    // let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
